feb20e793cd4430bcb069802046ea842f0ce4321
Robert Torres <robt792003@gmail.com>
2017-09-28 12:27:25 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="text-center" id="app">
+    <h1>Word Translator</h1>
+    <h5>Powered By Vue.js</h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+    <TranslateOutput v-text="translatedText"></TranslateOutput>
+
+
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
+
+export default {
+  name: 'app',
+  components: {
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function(){
+    return {
+      translatedText: ''
+    }
+  },
+  methods: {
+    translateText:function(text, language){
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170723T033042Z.51988b33e0c20fa4.0b07c6a87a3e5f02d8280de3b53434601dd26f38&lang='+language+'&text='+text)
+.then((response) => {
+  this.translatedText = response.body.text[0];
+});
+
+
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background:#fefefe;
+
+}
+</style>
---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2 class="text-success">{{translatedText}}</h2>
+</template>
+
+<script>
+export default {
+  name: 'TranslateOutput',
+  props: ['translatedText']
+}
+</script>
+
+<style>
+
+</style>
